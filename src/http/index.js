5f1d48238524acd74147c8dba28abb2426516c63
Amine Louni <amine.louni.dev@gmail.com>
2020-11-09 22:35:14 +0100

    prod links

--- src/http/index.js
+++ src/http/index.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export const auth = axios.create({
-  baseURL: "http://wsl:9000/api/v1/auth/",
+  baseURL: "https://young-bayou-54809.herokuapp.com//api/v1/auth/",
   headers: { "Content-type": "application/json" },
 });
 
 export const profile = axios.create({
-  baseURL: "http://wsl:9000/api/v1/profiles/",
+  baseURL: "https://young-bayou-54809.herokuapp.com//api/v1/profiles/",
   headers: { "Content-type": "application/json" },
 });
 export const user = axios.create({
-  baseURL: "http://wsl:9000/api/v1/users/",
+  baseURL: "https://young-bayou-54809.herokuapp.com//api/v1/users/",
   headers: { "Content-type": "application/json" },
 });
 export const post = axios.create({
-  baseURL: "http://wsl:9000/api/v1/posts/",
+  baseURL: "https://young-bayou-54809.herokuapp.com//api/v1/posts/",
   headers: { "Content-type": "application/json" },
 });
